0102a33d7571f98eaa0b94a02278f258f45bfbc4
voodoobenji <voodoobenjimusic@gmail.com>
2026-07-02 01:23:39 -0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/lib/auth";
+import { getBoard } from "@/lib/kv";
+import BoardView from "@/components/BoardView";
+
+export default async function HomePage() {
+  const session = await getServerSession(authOptions);
+  if (!session?.isMember) {
+    redirect("/login");
+  }
+  const board = await getBoard();
+  return <BoardView initialBoard={board} isAdmin={Boolean(session.isAdmin)} />;
+}
